f9c5e0ce9b67b65a4d36c4d978e8f3f394df9dd3
Yuri Alves de Almeida <yurialves2@gmail.com>
2018-05-10 19:45:13 -0300

    Habilitando CORS

--- config/server.js
+++ config/server.js
@@ -3,10 +3,12 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 // middlewares de formularios e json providos pelo body-parser
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}`)
